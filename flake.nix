3f8f0dca28863c8620846b6cbad924b12b60a9d7
MapoMagpie <zsyjk@live.cn>
2025-02-02 20:25:50 +0800

    chore

--- flake.nix
+++ flake.nix
@@ -1,25 +1,50 @@
 {
-  description = "A Nix-flake-based Node.js development environment";
+  description = "rimedm";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs =
-    { nixpkgs, ... }:
+    {
+      self,
+      nixpkgs,
+      systems,
+      ...
+    }:
     let
-      # system = "x86_64-darwin";
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+      pkgsFor = forEachSystem (system: import nixpkgs { inherit system; });
     in
     {
-      devShells."${system}".default = pkgs.mkShell {
-        packages = with pkgs; [
-          go
-          gopls
-          golangci-lint-langserver
-        ];
-        shellHook = "exec zsh";
-      };
+      formatter = forEachSystem (system: pkgsFor.${system}.alejandra);
+
+      devShells = forEachSystem (system: {
+        default = pkgsFor.${system}.mkShell {
+          packages = with pkgsFor.${system}; [
+            go
+            gopls
+            golangci-lint-langserver
+          ];
+          shellHook = "exec zsh";
+        };
+      });
+
+      packages = forEachSystem (system: {
+        default = pkgsFor.${system}.buildGoModule {
+          pname = "rimedm";
+          version = "1.0.10";
+          src = ./.;
+          vendorHash = null;
+        };
+      });
+
+      apps = forEachSystem (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/rimedm";
+        };
+      });
     };
 }
